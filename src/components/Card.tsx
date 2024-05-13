00e17d70a8450f8da964a3ceed941518219e0385
Majed <82qill@gmail.com>
2024-05-13 21:01:00 +0300

    Task 3 - Property Detail

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,26 @@
 import { Link } from "react-router-dom";
 import { Property } from "../utils/api";
-import Button from "./Button";
 
 interface CardProps extends Property {}
 
 const Card = ({ title, id, image, price, country }: CardProps) => {
   return (
-    <div className="bg-white rounded-md p-4 space-y-2">
-      <img
-        src={image}
-        className="w-full h-48 object-cover rounded-md"
-        alt="property"
-      />
-      <h1 className="font-bold">{title}</h1>
-      <div className="flex items-center justify-between">
-        <span>{price}$</span>
-        <span className=" text-ellipsis w-1/2 whitespace-nowrap overflow-hidden text-right">
-          {country}
-        </span>
+    <Link to={`/${id}`} className="cursor-pointer">
+      <div className="bg-white rounded-md p-4 space-y-2">
+        <img
+          src={image}
+          className="w-full h-48 object-cover rounded-md"
+          alt="property"
+        />
+        <h1 className="font-bold">{title}</h1>
+        <div className="flex items-center justify-between">
+          <span>{price}$</span>
+          <span className=" text-ellipsis w-1/2 whitespace-nowrap overflow-hidden text-right">
+            {country}
+          </span>
+        </div>
       </div>
-      <Link to={`/${id}`}>
-        <Button className="px-2 py-1 mt-2">View Property</Button>
-      </Link>
-    </div>
+    </Link>
   );
 };
 export default Card;
--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,11 +1,52 @@
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { Property, getProperty } from "../utils/api";
+import { useEffect, useState } from "react";
+import { formatDate } from "../utils/helper";
+import Button from "../components/Button";
 
 export default function Details() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<Property>();
+
+  useEffect(() => {
+    const fetchProperty = async () => {
+      setLoading(true);
+      try {
+        const response = await getProperty(id!);
+        setData(response);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProperty();
+  }, [id]);
+
   return (
     <main>
       <Navbar />
+      {loading || data == null ? (
+        <h1 className="text-center font-bold mt-2">Loading....</h1>
+      ) : (
+        <div className="w-full flex items-start flex-col mt-10 space-y-2">
+          <img
+            src={data.image}
+            className="w-full h-96 object-cover rounded-md"
+            alt="property"
+          />
+          <p className="text-sm text-black/40">{formatDate(data.createdAt)}</p>
+          <h1 className="font-bold text-2xl">{data.title}</h1>
+          <p className="text-black/50">{data.description}</p>
+          <span>{data.price}$</span>
+          <span>
+            {data.country} - {data.city} - {data.street}
+          </span>
+          <Button className="bg-red-600 hover:bg-red-500">Delete</Button>
+        </div>
+      )}
     </main>
   );
 }
